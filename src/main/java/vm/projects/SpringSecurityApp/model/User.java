80a4b5a9c8ad78f6fbb97b7588f43b4c622ebd89
Vladimir1Molchanov <92711094+Vladimir1Molchanov@users.noreply.github.com>
2022-05-19 11:35:00 +0300

    Add files via upload

---
+++ src/main/java/vm/projects/SpringSecurityApp/model/User.java
@@ -0,0 +1,82 @@
+package vm.projects.SpringSecurityApp.model;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "firstname")
+    private String firstName;
+    @Column(name = "lastName")
+    private String lastName;
+    @Column(name = "age")
+    private int age;
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "roles")
+    @ManyToMany(targetEntity = Role.class
+            , fetch = FetchType.LAZY
+    )
+    private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>(getRoles());
+    }
+
+    @Override
+    public String getUsername() {
+        return getFirstName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public static UserDetails fromUser(User User) {
+        return new org.springframework.security.core.userdetails.User(
+                User.getFirstName(), User.getPassword(),
+                User.isEnabled(),
+                User.isAccountNonExpired(),
+                User.isCredentialsNonExpired(),
+                User.isAccountNonLocked(),
+                User.getAuthorities()
+        );
+    }
+
+}
---
+++ src/test/java/vm/projects/SpringSecurityApp/SpringSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package vm.projects.SpringSecurityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
