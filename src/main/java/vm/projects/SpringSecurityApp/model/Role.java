78062eef11b4c497308536ea09f744b0fa24e0b6
Vladimir1Molchanov <92711094+Vladimir1Molchanov@users.noreply.github.com>
2022-05-19 13:26:42 +0300

    Add files via upload

---
+++ src/main/java/vm/projects/SpringSecurityApp/model/Role.java
@@ -0,0 +1,42 @@
+package vm.projects.SpringSecurityApp.model;
+
+import lombok.*;
+import org.hibernate.annotations.BatchSize;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "roleName")
+    private String roleName;
+    @Column(name = "users")
+    @ManyToMany(targetEntity = User.class
+            , mappedBy = "roles"
+            , fetch = FetchType.LAZY)
+    private List<User> users;
+
+    public Role(String roleName) {
+        this.roleName = "ROLE_" + roleName;
+    }
+
+    public String getRoleName() {
+        return new StringBuilder(roleName).substring(5);
+    }
+
+    @Override
+    public String getAuthority() {
+        return getRoleName();
+    }
+}
