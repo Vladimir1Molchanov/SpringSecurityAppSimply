716e08b3b72f4284c23b6fdc10218dabfff42a09
Vladimir1Molchanov <92711094+Vladimir1Molchanov@users.noreply.github.com>
2022-05-19 13:26:12 +0300

    Delete src directory

--- src/main/java/vm/projects/SpringSecurityApp/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package vm.projects.SpringSecurityApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vm.projects.SpringSecurityApp.model.User;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByFirstName(String name);
-}
